f9ad89bc37604543aa5ae95f2e66a7c8a7aa6d2e
cecilia <ceciliatangir@gmail.com>
2022-09-07 22:37:50 -0300

    fetch

---
+++ json/famoses.js
@@ -0,0 +1,14 @@
+[
+    {"signo": "Aries", "famosa": "Lady Gaga"},
+    {"signo": "Tauro","famosa": "Reina Isabel II"},
+    {"signo":"Géminis", "famosa": "Marilyn Monroe"},
+    {"signo":"Cáncer", "famosa": "Ariana Grande"},
+    {"signo":"Leo","famosa": "Dua Lipa"},
+    {"signo": "Virgo", "famosa": "Thalia"},
+    {"signo":"Libra", "famosa": "Rosalía"},
+    {"signo":"Escorpio","famosa": "Katy Perry"},
+    {"signo":"Sagitario", "famosa": "Miley Cyrus"},
+    {"signo":"Capricornio", "famosa": "Zooey Deschanel"},
+    {"signo":"Acuario", "famosa": "Shakira"},
+    {"signo":"Piscis", "famosa": "Rihanna"}
+]
